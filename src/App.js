da5afb31fc4db06d3a650b98f6c1634f2a4256df
restucode <restu.kersana1@gmail.com>
2024-10-31 13:30:19 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [rates, setRates] = useState([])
+  // https://api.currencyfreaks.com/v2.0/rates/latest?apikey=64d4ca524760478fb1ce6316388a3e6f&symbols=CAD,EUR,IDR,JPY,CHF,GBP
+  useEffect(() => {
+    axios.get("https://api.currencyfreaks.com/v2.0/rates/latest", {
+      params: {
+        'apikey' : '64d4ca524760478fb1ce6316388a3e6f',
+        'symbols' : 'CAD,EUR,IDR,JPY,CHF,GBP'
+      }
+    })
+    .then(res => {
+      console.log(res.data.rates)
+      setRates(res.data.rates)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <table>
+          <thead>
+            <tr>
+              <th>Currency</th>
+              <th>We Buy</th>
+              <th>Exchange Rate</th>
+              <th>We Sell</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            Object.entries(rates).map(([cur,excrate], index) => (
+              <tr key={index}>
+                <td>{cur}</td>
+                <td>{parseFloat(excrate) + parseFloat((excrate * 0.05))}</td>
+                <td>{excrate}</td>
+                <td>{parseFloat(excrate) - parseFloat((excrate * 0.05))}</td>
+              </tr>
+            ))
+          }
+          
+          </tbody>
+        </table>
+        <div className="copyright">
+          <p>Rate are based from 1 USD</p>
+          <p>This application uses API from https://currencyfreaks.com</p>
+        </div>
+      
+      </div>
     </div>
   );
 }
